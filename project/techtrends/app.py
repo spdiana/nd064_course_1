975895f62243312e7b52875863fe3b1e87a45c59
spdiana <spdiana@gmail.com>
2021-08-28 19:43:01 +0300

    update app.py, logs, add screenshots

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -4,11 +4,15 @@ import logging
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+total_db_connections = 0
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    global total_db_connections
+    total_db_connections += 1
     return connection
 
 # Function to get a post using its ID
@@ -46,11 +50,11 @@ def status():
 def metrics():
     posts = get_total_posts()
     response = app.response_class(
-            response=json.dumps({"db_connection_count": 1, "post_count": posts[0]}),
+            response=json.dumps({"db_connection_count": total_db_connections, "post_count": posts[0]}),
             status=200,
             mimetype='application/json'
     )
-    app.logger.info('Metrics request successful')
+    app.logger.info('Metrics request successful, "%s" total_db_connections, "%s" post_count', total_db_connections, posts[0])
     return response
 
 # Define the main route of the web application 
@@ -67,13 +71,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info('Post not found - 404')
       return render_template('404.html'), 404
     else:
+      app.logger.info('Article "%s" retrieved!', post['title'])
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('About Us" page is retrieved')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -91,15 +98,11 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            app.logger.info('Article "%s" saved!', title)
             return redirect(url_for('index'))
-
     return render_template('create.html')
 
 # start the application on port 3111
 if __name__ == "__main__":
-    #logging.basicConfig(format='%(asctime)s %(levelname)s {%(module)s} [%(funcName)s] %(message)s',
-    #                     datefmt='%Y-%m-%d,%H:%M:%S:%f', filename='app.log', level=logging.DEBUG)
-   #logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='app.log',level=logging.DEBUG)
    app.run(host='0.0.0.0', port='3111', debug = True)
